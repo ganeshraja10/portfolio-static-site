2056e6a1d9ea6cc2fd33feb8b267379c727c1f80
Ganesh <ganeshraja1010@gmail.com>
2020-05-12 20:17:45 +0530

    Added static files

---
+++ precache-manifest.f0c8888bb9c55f0c8e2f6dd38ca98b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b9acca504b2422b13b1bb53bb4a463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71b9e6944677917dbfb5",
+    "url": "/static/js/2.58146a11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.58146a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575764d3cdada29adb27",
+    "url": "/static/js/main.60a205f5.chunk.js"
+  },
+  {
+    "revision": "8151b6e8ecddb53b288b",
+    "url": "/static/js/runtime-main.a98ec1ca.js"
+  }
+]);
